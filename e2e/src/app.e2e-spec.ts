457752c6907238ae1dbac0f796fdd859a953dafe
s.yamamoto <hear5goes2nothing5@gmail.com>
2018-09-15 11:00:05 +0900

    reset init

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('ng-chat App', () => {
+describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +9,6 @@ describe('ng-chat App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to NgChat!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,10 @@
 import { Component } from '@angular/core';
-import { Comment, User } from './class/chat';
-import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
-
-const CURRENT_USER: User = new User(1, 'Tanaka Jiro');
-const ANOTHER_USER: User = new User(2, 'Suzuki Taro');
 
 @Component({
   selector: 'app-root',
-  template: `
-  	<app-header></app-header>
-  	<router-outlet></router-outlet>
-  `,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  constructor() { 
-  }
-
+  title = 'NgChat';
 }
